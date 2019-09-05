1cb14dc95d13237a8ed25266c19d6bada5a1e422
Anjolaoluwa Bassey <anjolaoluwabassey@Anjola-2.local>
2019-09-05 15:47:42 +0100

    add signup link

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
   let contentHolder = $("#content");
-  let user;
+  let user, reference, token;
   let flwAuthToken;
   let API_publicKey;
   let API_secretKey;
+  let pubKey = "FLWPUBK_TEST-0db01907c1b990c273c365a696c1613d-X";
+  let secKey = "FLWSECK_TEST-624d8f04393b01cac90d02f562b26389-X";
   let loginButton = $("#login");
   let getKeys = $("#submit");
+  let payButton = $("#pay");
+  let chargeTitle = $("#charge");
+  let verifyTitle = $("#verify");
+  let paymentplanTitle = $("#paymentplan");
+  let tokenizeTitle = $("#tokenize");
+  let successTitle = $("#success");
+  let codebox = $(".codebox");
+  let codeboxBanner = $("#banner");
 
   var copyButton = $("<button/>", {
     text: "Copy",
@@ -38,6 +48,9 @@ $(document).ready(function() {
 
       $("#copied").val(content);
 
+      API_publicKey = window.localStorage.getItem("API_publicKey");
+      API_secretKey = window.localStorage.getItem("API_secretKey");
+
       var pub = content.search("pin");
       var sec = content.search("ravepay");
       if (pub > 0) {
@@ -57,7 +70,7 @@ $(document).ready(function() {
       copyArea.remove();
     });
 
-  $(".left-nav").on("click", ".get-content", function (e) {
+  $(".left-nav").on("click", ".get-content", function(e) {
     $("#log").html("");
     e.preventDefault();
     var addedUrl = $(this).attr("id");
@@ -86,14 +99,13 @@ $(document).ready(function() {
         // console.log($("pre").html());
         $("pre").addClass("highlight");
 
-          html = $.parseHTML(md.render(con)),
-          nodeNames = [];      
+        (html = $.parseHTML(md.render(con))), (nodeNames = []);
 
-        $.each(html, function (i, el) {
+        $.each(html, function(i, el) {
           if (el.nodeName == "H2") {
-            nodeNames[i] = '<li class="listing"><a href="#h2">' + el.innerText + "</li></a>";
+            nodeNames[i] =
+              '<li class="listing"><a href="#h2">' + el.innerText + "</li></a>";
           }
-         
         });
 
         $("#log").append("<h4>TABLE OF CONTENTS</h4>");
@@ -145,6 +157,12 @@ $(document).ready(function() {
     $(".ais-search-box--input").focus();
   });
 
+  // displaying the right transfer pages when the right nav is clicked
+  $(".right-nav").on("change", ".transfer-select", function(e) {
+    var gg = $(this).attr("class");
+    console.log(gg);
+  });
+
   // When the user clicks anywhere outside of the modal, close it
   $(document).on("click", function(event) {
     // console.log(event.target.className);
@@ -154,7 +172,7 @@ $(document).ready(function() {
     }
   });
 
-  $(".modal-close").click(function () {
+  $(".modal-close").click(function() {
     console.log("gvh");
     $(".searchModal").removeClass("show");
     $(".searchModal").addClass("hide");
@@ -199,7 +217,6 @@ $(document).ready(function() {
     if (password.length < 1) {
       $("#password").after('<span class="error">This field is required</span>');
     }
-
 
     let requestObject = {
       identifier: email,
@@ -248,7 +265,7 @@ $(document).ready(function() {
       }
     });
 
-    loggedIn.done(function () {
+    loggedIn.done(function() {
       $.ajax({
         url: "https://api.ravepay.co/merchant/accounts/update",
         type: "post",
@@ -337,4 +354,182 @@ $(document).ready(function() {
       });
     });
   });
+
+  // function to power adding rave to your project section
+  payButton.click(function() {
+
+    var x = getpaidSetup({
+      PBFPubKey: pubKey,
+      customer_email: "user@example.com",
+      amount: 2000,
+      customer_phone: "234099940409",
+      currency: "NGN",
+      txref: Date.now(),
+      meta: [
+        {
+          metaname: "flightID",
+          metavalue: "AP1234"
+        }
+      ],
+      onclose: function() {},
+      callback: function(response) {
+        reference = response.tx.txRef; // collect txRef returned and pass to a server page to complete status check.
+        console.log("This is the response returned after a charge", response);
+        if (
+          response.tx.chargeResponseCode == "00" ||
+          response.tx.chargeResponseCode == "0"
+        ) {
+          // redirect to a success page
+          chargeTitle.removeClass("text-current").addClass("text-completed");
+          $("#charge span")
+            .removeClass("trynow-current")
+            .addClass("trynow-completed");
+          verifyTitle.removeClass("text-waiting").addClass("text-current");
+          $("#verify span")
+            .removeClass("trynow-waiting")
+            .addClass("trynow-current");
+
+          codebox.html(
+            `Here is a sample code snippet <br>
+            <code>
+            curl --request POST \ <br>
+              --url https://api.ravepay.co/flwv3-pug/getpaidx/api/v2/verify \ <br>
+              --header 'content-type: application/json' \ <br>
+              --data '{"txref":"${reference}","SECKEY":"${secKey}"}'
+            </code>`
+          );
+          codebox.append(
+            "<button id='verifyPayment' class='btn'>Verify Payment</button>"
+          );
+          codeboxBanner.text(
+            "Your card has been charged successfully. Now we verify the payment to get our token for tokenized charges."
+          );
+          $("#note").html(
+            `you can access the token like this: <br> token = response.data.card.card_tokens.embedtoken;`
+          );
+        } else {
+          // redirect to a failure page.
+        }
+
+        x.close(); // use this to close the modal immediately after payment.
+      }
+    });
+  });
+
+  // verifying the transaction when the verify payment button is clicked
+  $("body").on("click", "#verifyPayment", function() {
+    console.log("verifying");
+    $.post(
+      "https://api.ravepay.co/flwv3-pug/getpaidx/api/v2/verify",
+      {
+        txref: reference,
+        SECKEY: secKey
+      },
+      function(data, status) {
+        token = data.data.card.card_tokens[0].embedtoken;
+
+        paymentplanTitle.removeClass("text-waiting").addClass("text-current");
+        $("#paymentplan span")
+          .removeClass("trynow-waiting")
+          .addClass("trynow-current");
+        verifyTitle.removeClass("text-current").addClass("text-completed");
+        $("#verify span")
+          .removeClass("trynow-current")
+          .addClass("trynow-completed");
+
+        codebox.html(
+          `curl --request POST \ <br>
+          --url https://api.ravepay.co/v2/gpx/paymentplans/create \ <br>
+          --data '{"amount":"100","name":"Gate Waters","interval":"hourly","duration":3,"seckey":"${secKey}"}'`
+        );
+        codebox.append(
+          "<button id='createPlan' class='btn'>Create Plan</button>"
+        );
+        codeboxBanner.text(
+          "On successful verification, we will store the token for future tokenized charges. We now have to create a plan for subscriptions"
+        );
+      }
+    );
+  });
+
+  // create a payment plan when the payment plan button is clicked
+  $("body").on("click", "#createPlan", function() {
+    $.post(
+      "https://api.ravepay.co/v2/gpx/paymentplans/create",
+      {
+        amount: "100",
+        name: "Gate Waters",
+        interval: "hourly",
+        duration: "3",
+        seckey: secKey
+      },
+      function(data, status) {
+        planId = data.data.id;
+
+        tokenizeTitle.removeClass("text-waiting").addClass("text-current");
+        $("#tokenize span")
+          .removeClass("trynow-waiting")
+          .addClass("trynow-current");
+        paymentplanTitle.removeClass("text-current").addClass("text-completed");
+        $("#paymentplan span")
+          .removeClass("trynow-current")
+          .addClass("trynow-completed");
+
+        codebox.html(`<div class="notes-padding">curl --request POST \
+          --url https://api.ravepay.co/flwv3-pug/getpaidx/api/tokenized/charge \ <br>
+          --data '{"SECKEY":"${secKey}","duration":"3","interval":"hourly","name":"Gate Waters","amount":"100"}'</div>`);
+        codebox.append(
+          "<button id='tokenizedCharge' class='btn'>Tokenized Recurring Charge</button>"
+        );
+        codeboxBanner.text(
+          "You have successfully created a payment plan, you can now add it to your charge payload to start a subscription."
+        );
+        codebox.append("<div id='note'></div>");
+        $("#note").html(
+          "You can access the payment plan like this: var planId = data.data.id;<br> you can also add the token gotten earlier to charge our saved card"
+        );
+      }
+    );
+  });
+
+  // tokenized recurring charge
+  $("body").on("click", "#tokenizedCharge", function() {
+    $.post(
+      "https://api.ravepay.co/flwv3-pug/getpaidx/api/tokenized/charge",
+      {
+        SECKEY: secKey,
+        name: "Gate Waters",
+        token: token,
+        currency: "NGN",
+        country: "NG",
+        amount: 300,
+        email: "user@example.com",
+        txRef: "MC-835468",
+        payment_plan: planId
+      },
+      function(data, status) {
+        console.log(data);
+
+        tokenizeTitle.removeClass("text-current").addClass("text-completed");
+        $("#tokenize span")
+          .removeClass("trynow-current")
+          .addClass("trynow-completed");
+        successTitle.removeClass("text-waiting").addClass("text-current");
+        $("#success span")
+          .removeClass("trynow-waiting")
+          .addClass("trynow-current");
+
+        codebox.html(`finished`);
+        codebox.remove(
+          "<button id='tokenizedCharge' class='btn'>Tokenized Recurring Charge</button>"
+        );
+        codeboxBanner.text(
+          "You have successfully added rave to your project by charging a card, saving that card and creating a subscription for the user"
+        );
+        
+      }
+    );
+  });
+
+
 });
